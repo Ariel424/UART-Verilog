040ee5983b6e175f6546a38852fd05b99b7bf93e
Ariel Topaz <130974714+Ariel424@users.noreply.github.com>
2025-10-30 12:16:23 +0200

    Create Verification code.v

---
+++ Verification code.v
@@ -0,0 +1,10 @@
+/verification_project
+
+    /design          --> your RTL/VHDL/Verilog files
+    /tb              --> all testbenches
+        /env         --> environment class
+        /agent       --> agents
+        /monitor     --> monitors
+        /scoreboard  --> scoreboard
+        /sequence    --> sequences
+    /sim             --> simulation scripts, waveforms, logs
